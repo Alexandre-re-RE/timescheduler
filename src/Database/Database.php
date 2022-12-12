332873085115e02bd9310e4f4a2faaa416cc7294
Alexandre <webmaster@alexandre.re>
2022-12-12 14:54:19 +0400

    création object de connection à la base de données

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -1,7 +1,37 @@
 <?php
 
-namespace Database;
+namespace App\Database;
+
+use ErrorException;
+use Exception;
+use PDO;
+
 class Database
 {
+    const DEFAULT_DATABASE = 'timescheduler';
+    const DEFAULT_USER = 'root';
+    const DEFAULT_PASS = '';
+
+    private $db;
+
+    public function __construct(
+        $dbname = self::DEFAULT_DATABASE,
+        $dbuser = self::DEFAULT_USER,
+        $dbpass = self::DEFAULT_PASS
+    ) {
+        try {
+            $this->db = new PDO("mysql:host=localhost:3306;dbname=$dbname", $dbuser, $dbpass);
+        } catch (Exception $e) {
+            echo($e->getMessage());
+        }
+    }
+
+    public function getDb()
+    {
+        if(!$this->db instanceof PDO) {
+            throw new ErrorException('unable to connect to db');
+        }
+        return $this->getDb();
+    }
 
 }
