16a829613a366de3ed7eb0c824ba955dbbaa4430
kotou-boina <kotou.boina@gmail.com>
2022-12-13 10:43:56 +0100

    update client entity

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -2,59 +2,114 @@
 
 namespace App\Entity;
 
+use DateTime;
+
 /**
  * Client class
  * 
  * @property int $id
  * @property string $name
+ * @property DateTime $created_at
+ * @property DateTime $updated_at
  */
-    class Client{
-        private $id;
-        private $name;
+class Client
+{
+    private $id;
+    private $name;
+    private $created_at;
+    private $updated_at;
 
-        public function __construct()
-        {
-
-        }
-
-        /**
-         * Get the value of id
-         */ 
-        public function getId()
-        {
-                return $this->id;
-        }
-
-        /**
-         * Set the value of id
-         *
-         * @return  self
-         */ 
-        public function setId($id)
-        {
-                $this->id = $id;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of name
-         */ 
-        public function getName()
-        {
-                return $this->name;
-        }
-
-        /**
-         * Set the value of name
-         *
-         * @return  self
-         */ 
-        public function setName($name)
-        {
-                $this->name = $name;
-
-                return $this;
-        }
+    public function __construct()
+    {
+        $this->created_at = new DateTime(is_string($this->created_at) ? $this->created_at : '');
+        $this->updated_at = new DateTime(is_string($this->updated_at) ? $this->updated_at : '');
     }
 
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     *
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set the value of created_at
+     *
+     * @param mixed  $created_at 
+     *
+     * @return self
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updated_at
+     *
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set the value of updated_at
+     *
+     * @param mixed  $updated_at 
+     *
+     * @return self
+     */
+    public function setUpdatedAt($updated_at)
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
