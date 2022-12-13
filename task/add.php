97d89092c0274218c31a8bdbeef103e502a46c60
Tony RANGUIN ASSATI <tranguin.cda@FORMATION.LOCAL>
2022-12-13 16:32:39 +0400

    c'est pas fini

--- task/add.php
+++ task/add.php
@@ -5,43 +5,63 @@ use App\Entity\Task;
 include_once("../src/Entity/Task.php");
 require("../bootstrap.php");
 
-function ajouter($title, $description, $priority, $startDate, $realStartDate, $endDate, $realEndDate, PDO $db)
+function ajouter($title, $description, $priority, $startDate, $realStartDate, $endDate, $db)
 {
     session_start();
     //créations de l'objet
     $task = new Task();
-    //add dans l'objet
-    $task->title  = $title;
-    $task->description = $description;
-    $task->priority = $priority;
-    $task->start_date = $startDate;
-    $task->end_date = $endDate;
-    $task->real_start_date = $realStartDate;
-    $task->real_end_date = $realEndDate;
-    $task->create = date("Y/m/d");
-    $task->updated_at =  date("Y/m/d");
+    $task
+        ->setTitle($title)
+        ->setDescription($description)
+        ->setPriority($priority)
+        ->setStartDate($startDate)
+        ->setRealStartDate($realStartDate)
+        ->setEndDate($endDate);
 
-    //créations d'une requetes pour récup l'id du project 
-    $reqProject = $db->query("Select id from projects where client_id=:clientId");
-    $reqProject->bindParam("clientId", $_SESSION["user"]);
-    $reqProject->execute();
-    $reqProject->setFetchMode(PDO::FETCH_ASSOC);
-    $reqProject = $reqProject->fetchAll();
+    //créations d'une requetes pour récup l'id du project mais ne brute
+    $client = 1;
+    $user = 1;
+    $priority = 1;
+    $reqProject = $db->prepare("Select id from projects where client_id=:clientId");
+    // $reqProject->bindParam("clientId", $_SESSION["client"]);
+    $reqProject->bindParam(":clientId", $client);
+    $reqProject->fetch(\PDO::FETCH_ASSOC);
+    // $res =  $reqProject->execute();
+    $res = 1;
     //persitence des données 
     $status = 1;
-    $date = date("Y/m/d");
-    $req = $db->prepare("INSERT INTO `task`(`title`, `description`, `priority`, `start_date`, `end_date`, `real_start_date`, `status_id`,'user_id','project_id','created_at','updated_at') VALUES (:title,:descrip,:priority,:startDate,:endDate,:realStartDate,:statusID,:userID,:projectId,:createdAt,:updatedAt)");
-    $req->bindParam("tiltle", $_POST['inputTitle']);
-    $req->bindParam("descrip", $_POST['inputDescription']);
-    $req->bindParam("priority", $_POST['inputPriority']);
-    $req->bindParam("startDate", $_POST['inputStartDate']);
-    $req->bindParam("endDate", $_POST['inputEndDate']);
-    $req->bindParam("realStartDate", $_POST['inputRealStartDate']);
-    $req->bindParam("statusId", $status);
-    $req->bindParam("userId", $_SESSION["user"]);
-    $req->bindParam("projectID", $reqProject);
-    $req->bindParam("createdAt", $date);
-    $req->bindParam("updatedAt", $date);
-    $req->execute();
-    //on récup cmt  par session user ,   je réalise une requete project ,status par défaut == en cours 
+    $date = date("Y-m-d");
+    // $req = $db->prepare("INSERT INTO tasks (title, description,priority,start_date,end_date,real_start_date,status_id,user_id,project_id,created_at,updated_at) VALUES (:title,':description,:priority,:startDate,:endDate,:realStartDate,:statusId,:userId,:projectId,:createdAt,:updatedAt)");
+
+    $req = $db->prepare("INSERT INTO tasks (title, description) VALUES (:title,:description)");
+
+    /*$req->bindParam(":title", $_POST['inputTitle']);
+    $req->bindParam(":descrip", $_POST['inputDescription']);
+    $req->bindParam(":priority", $priority);
+    $req->bindParam(":startDate", $date);
+    $req->bindParam(":endDate", $date);
+    $req->bindParam(":realStartDate", $date);
+    $req->bindParam(":statusId", $status);
+    $req->bindParam(":userId", $user);
+    $req->bindParam(":projectId", $res);
+    $req->bindParam(":createdAt", $date);
+    $req->bindParam(":updatedAt", $date);*/
+    $req->execute(array(
+        ':title' => $title,
+        ':description' => $description
+    ));
 }
+
+// foreach ($_POST as $key => $value) {
+//     echo "<br> $key=>$value";
+//     ${$key} = $value;
+// }
+
+$title = $_POST["inputTitle"];
+$description = $_POST["inputDescription"];
+$priority = $_POST["inputPriority"];
+$startDate = $_POST["inputStartDate"];
+$realStartDate = $_POST["inputStartDate"];
+$endDate = $_POST["inputEndDate"];
+ajouter($title, $description, $priority, $startDate, $realStartDate, $endDate, $db);
+header("location:form.php");
