72f5c866e112065133e9ca0bd01267976355df7c
kotou-boina <kotou.boina@gmail.com>
2022-12-13 11:08:57 +0100

    fix paths

--- paths.php
+++ paths.php
@@ -1,9 +1,9 @@
 <?php
 
-define('ROOT', dirname(__DIR__));
+define('ROOT', __DIR__);
 
 define('APP', ROOT . DIRECTORY_SEPARATOR . 'src');
 
 define('APP_DIR', str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace(DIRECTORY_SEPARATOR, '/', ROOT)) . '/');
 
-define('TEMPLATES', APP . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR);
+define('TEMPLATES', APP . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR);
