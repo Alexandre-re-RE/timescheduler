71c314fc8133ee10b084710419b5e29ab942b96d
Tony RANGUIN ASSATI <tranguin.cda@FORMATION.LOCAL>
2022-12-14 11:11:25 +0400

    fix config APP

--- paths.php
+++ paths.php
@@ -7,3 +7,5 @@ define('APP', ROOT . DIRECTORY_SEPARATOR . 'src');
 define('APP_DIR', str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace(DIRECTORY_SEPARATOR, '/', ROOT)) . '/');
 
 define('TEMPLATES', APP . DIRECTORY_SEPARATOR . 'Templates' . DIRECTORY_SEPARATOR);
+
+define('CONFIG_FILE', ROOT . DIRECTORY_SEPARATOR . 'config/app.php');
